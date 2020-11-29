52a0c72527e3f1cdcea535b69a1c37353126f11a
sarawarnock <sara@sarawarnock.com>
2020-11-29 12:56:49 -0700

    login-auth

---
+++ src/Auth-Login/auth-service.js
@@ -0,0 +1,7 @@
+// GET https://accounts.spotify.com/authorize
+// get request sent to authorize endpoint
+
+// parameters: client_id, response_type, redirect_uri, state
+// optional: scope, show_dialog (default = false)
+
+// Example request: GET https://accounts.spotify.com/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09
