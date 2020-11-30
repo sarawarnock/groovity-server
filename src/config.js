16b327d813710f11390427934abcb2300f9acb58
sarawarnock <sara@sarawarnock.com>
2020-11-30 09:36:01 -0700

    auth-login route

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    REDIRECT: process.env.REDIRECT,
 }
